985eb50855361b40d3cad5b1ea219d917ad9ef43
Alen Sendro <sendro.alen@nsoft.com>
2020-04-17 02:39:08 +0200

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/footer.scss'
+
+function Footer() {
+
+  return (
+    <div className="footer">
+        <div className="footer-text">
+            Made with love by&nbsp;<a href="https://github.com/dev387">dev387</a>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
