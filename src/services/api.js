6c7abca05abf7b3c6e39927c039abe5df260bf9e
Alen Sendro <sendro.alen@nsoft.com>
2020-04-18 00:20:35 +0200

    Add getPrayerTimes to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,19 @@
 const api = 'https://api.vaktija.ba/vaktija/v1';
+
 const getLocations = () => {
   return fetch(`${api}/lokacije`).then(response => response.json());
 };
 
+const getPrayerTimes = ({
+  day = (new Date()).getDate(),
+  month = (new Date()).getMonth() + 1,
+  year = (new Date()).getFullYear(),
+  location
+ }) => {
+  return fetch(`${api}/${location}/${year}/${month}/${day}`).then(response => response.json());
+};
+
 export default {
   getLocations,
+  getPrayerTimes,
 };
