bd60387ea0ad15378d01b9f5240d64e768b78e3c
Alen Sendro <sendro.alen@nsoft.com>
2020-04-18 02:12:33 +0200

    Cache data - fetch data only one time per day if location is not changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../services/api';
+import helpers from '../services/helpers';
 import Header from './Header';
 import Prayer from './Prayer';
 import Footer from './Footer';
@@ -7,20 +8,26 @@ import PrayerModel from '../models/Prayer';
 import '../styles/app.scss'
 
 function App() {
-  const [locations, setLocations] = useState([]);
-  const [selectedLocation, setLocation] = useState(61);
-  const [prayerTimes, setPrayerTimes] = useState([]);
+  const data = helpers.getData();
+
+  const [locations, setLocations] = useState(data.locations || []);
+  const [selectedLocation, setLocation] = useState(data.selectedLocation || 61);
+  const [prayerTimes, setPrayerTimes] = useState(data.prayers || []);
+  const [date] = useState(helpers.getFullDate(new Date()))
 
   useEffect(() => {
-    api.getLocations().then((response) => {
-      setLocations(response);
-      localStorage.setItem('locations', JSON.stringify(response));
+    if(data.selectedLocation !== selectedLocation || date !== data.date) {
+      api.getLocations().then((response) => {
+        setLocations(response);
 
-      api.getPrayerTimes({ location: selectedLocation }).then((prayers) => {
-        setPrayerTimes(prayers.vakat);
+        api.getPrayerTimes({ location: selectedLocation }).then((prayers) => {
+          setPrayerTimes(prayers.vakat);
+
+          helpers.storeData({locations: response, prayers: prayers.vakat, selectedLocation });
+        });
       });
-    });
-  }, [selectedLocation]);
+    }
+  }, [selectedLocation, data.selectedLocation, data.date, date]);
 
   const prayers = prayerTimes.map((salah, id) => new PrayerModel({ time: salah, id: id}));
 
--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,34 +1,48 @@
 const storeData = ({ locations, selectedLocation, prayers }) => {
   localStorage.setItem('locations', JSON.stringify(locations));
-  localStorage.setItem('selectedLocation', JSON.stringify(prayers));
+  localStorage.setItem('selectedLocation', JSON.stringify(selectedLocation));
   localStorage.setItem('prayers', JSON.stringify(prayers));
+  localStorage.setItem('date', JSON.stringify(getFullDate(new Date())));
 };
 
 const getData = () => {
   let locations = localStorage.getItem('locations');
   let selectedLocation = localStorage.getItem('selectedLocation');
   let prayers = localStorage.getItem('prayers');
+  let date = localStorage.getItem('date');
 
-  if(locations !== null) {
+  if (locations !== null) {
     locations = JSON.parse(locations)
   }
 
-  if(selectedLocation !== null) {
+  if (selectedLocation !== null) {
     selectedLocation = JSON.parse(selectedLocation)
   }
 
-  if(prayers !== null) {
+  if (prayers !== null) {
     prayers = JSON.parse(prayers)
   }
+
+  if (date !== null) {
+    date = JSON.parse(date)
+  }
+
+  if (prayers === null || selectedLocation === null || locations === null || date === null) return false;
 
   return {
     locations,
     selectedLocation,
     prayers,
+    date,
   };
+};
+
+const getFullDate = (date) => {
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
 };
 
 export default {
   storeData,
   getData,
+  getFullDate,
 };
