11616cc23fe7e3fa5d965ec2d3e1853cd604b309
Alen Sendro <sendro.alen@nsoft.com>
2020-04-19 02:02:19 +0200

    Send test notification

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Header from './Header';
 import Prayer from './Prayer';
 import Footer from './Footer';
 import PrayerModel from '../models/Prayer';
+import notifications from '../services/notifications';
 import '../styles/app.scss'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
   const [selectedLocation, setLocation] = useState(data.selectedLocation || 61);
   const [prayerTimes, setPrayerTimes] = useState(data.prayers || []);
   const [date] = useState(helpers.getFullDate(new Date()))
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
 
   useEffect(() => {
     if(data.selectedLocation !== selectedLocation || date !== data.date) {
@@ -29,6 +31,12 @@ function App() {
     }
   }, [selectedLocation, data.selectedLocation, data.date, date]);
 
+  useEffect(() => {
+    if(notificationsEnabled) {
+      notifications.showNotification({ title: 'Vaktija', body: 'Uskoro ce aksam' });
+    }
+  }, [notificationsEnabled]);
+
   const prayers = prayerTimes.map((salah, id) => new PrayerModel({ time: salah, id: id}));
 
   const prayersWrap = prayers.map((prayer) => {
@@ -43,6 +51,11 @@ function App() {
         locations={locations}
         selected={selectedLocation}
         onChangeSelected={(e) => {setLocation(e.target.value)}}/>
+
+        <button onClick={() => {
+          setNotificationsEnabled(notifications.requestNotifications());
+        }}>Omoguci obavijesti</button>
+
       <div className="prayers">
         {prayersWrap}
       </div>
