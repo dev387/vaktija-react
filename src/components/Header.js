ca65f339e782fdccfcafeeee8247074f60071f18
Alen Sendro <sendro.alen@nsoft.com>
2020-04-30 00:16:05 +0200

    Move menu components from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Menu from './Menu';
+import helpers from '../services/helpers';
+import api from '../services/api';
 
 import '../styles/header.scss'
 
-function Header({ locations, selected, onChangeSelected }) {
-  const location = locations[selected] || 'Mostar';
+function Header({ onChangeSelected }) {
+  const data = {
+    locations: helpers.getDataByKey('locations'),
+    selectedLocation: helpers.getDataByKey('selectedLocation'),
+  }
+  const [selectedLocation, setLocation] = useState(data.selectedLocation || 61);
+  const [locations, setLocations] = useState(data.locations || []);
+  const location = locations[selectedLocation] || 'Mostar';
   const currentDate = new Date();
+
+  const changeLocation = (e) => {
+    setLocation(e.target.value);
+    onChangeSelected(e.target.value);
+    helpers.storeDataByKey({ key: 'selectedLocation', value: e.target.value });
+  };
+
+  useEffect(() => {
+    if (!locations || !locations.length) {
+      api.getLocations().then((response) => {
+        setLocations(response);
+        helpers.storeDataByKey({ key: 'locations', value: response });
+      });
+    }
+  }, [locations]);
 
   return (
     <div className="header font-alt">
+      <div className="navigation">
+        <Menu />
+      </div>
       <div className="header-info">
         {location}, {currentDate.toDateString()}
       </div>
-      <div className="locations-wrapper">
-        {
-          locations.length ?
-            (<>
-              <select
-                id="locations"
-                name="locations"
-                defaultValue={selected}
-                onChange={onChangeSelected}
-                >
-                {locations.map(
-                  (location, id) => <option key={id} value={id}>{location}</option>
-                )}
-              </select>
-              <div className="select-icon"></div>
-            </>)
-            :
-            ('Ucitavanje...')
-        }
-      </div>
+      {
+        locations.length ?
+          (<>
+            <select
+              className="form-control"
+              name="locations"
+              defaultValue={selectedLocation}
+              onChange={changeLocation}
+            >
+              {locations.map(
+                (location, id) => <option key={id} value={id}>{location}</option>
+              )}
+            </select>
+          </>)
+          :
+          ('Ucitavanje...')
+      }
     </div>
   );
 }
